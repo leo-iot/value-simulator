d224cb30af85ea828bd5c2672c7955879f89c29d
VinzentKronreif <vini.kro@gmail.com>
2022-01-25 13:32:34 +0100

    update endpoint to return Response instead of boolean

--- src/main/java/at/htl/mqtt/client/boundary/ConfigEndpoint.java
+++ src/main/java/at/htl/mqtt/client/boundary/ConfigEndpoint.java
@@ -5,6 +5,8 @@ import at.htl.mqtt.client.repository.RoomRepository;
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
 
 @Path("/api")
 public class ConfigEndpoint {
@@ -16,8 +18,14 @@ public class ConfigEndpoint {
     @Path("addRoom/{roomName}")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public boolean addRoom(@PathParam("roomName") String roomName) {
-        return roomRepo.addRoom(roomName);
+    public Response addRoom(@PathParam("roomName") String roomName) {
+        boolean success = roomRepo.addRoom(roomName);
+
+        if (success){
+            return Response.status(201).build();
+        }
+
+        return Response.status(400).build();
     }
 
     @POST
--- src/test/java/at/htl/mqtt/client/boundary/ConfigEndpointTest.java
+++ src/test/java/at/htl/mqtt/client/boundary/ConfigEndpointTest.java
@@ -2,10 +2,6 @@ package at.htl.mqtt.client.boundary;
 
 import com.intuit.karate.junit5.Karate;
 import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Test;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 public class ConfigEndpointTest {
