ea3a29ada58f16a1b81a5cdf7f18506a13cb89f4
VinzentKronreif <vini.kro@gmail.com>
2022-01-26 23:31:30 +0100

    tried Runner API from Karate

--- src/test/java/at/htl/mqtt/client/boundary/ConfigEndpointTest.java
+++ src/test/java/at/htl/mqtt/client/boundary/ConfigEndpointTest.java
@@ -1,13 +1,20 @@
 package at.htl.mqtt.client.boundary;
 
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
 import com.intuit.karate.junit5.Karate;
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 @QuarkusTest
 public class ConfigEndpointTest {
 
     @Karate.Test
-    Karate testGetRoom() {
-        return Karate.run("config-resource.feature").relativeTo(getClass());
+    Karate testRoom() {
+        return Karate.run("config-resource.feature").relativeTo(getClass()).systemProperty("engine.WarnInterpreterOnly", "true");
+        //Results results = Runner.path("config-resource.feature").relativeTo(getClass()).parallel(5);
+        //assertEquals(0,results.getFailCount(), results.getErrorMessages());
     }
 }
