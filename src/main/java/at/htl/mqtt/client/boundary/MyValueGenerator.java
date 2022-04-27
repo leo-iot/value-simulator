361ed3e59a09656ccd3e9d76ba9af41666c9c9d8
Jakob Rathberger <jakob.rathberger@gmail.com>
2022-04-27 23:40:29 +0200

    change period to 5 sec #leoiot-108

--- src/main/java/at/htl/mqtt/client/boundary/MyValueGenerator.java
+++ src/main/java/at/htl/mqtt/client/boundary/MyValueGenerator.java
@@ -32,7 +32,7 @@ public class MyValueGenerator {
     private List<Double> goodTemps = new LinkedList<>();
 
     public void roomData(Room room){
-        subscriptions.put(room.getName(), Observable.interval(0, 10, TimeUnit.SECONDS)
+        subscriptions.put(room.getName(), Observable.interval(0, 5, TimeUnit.SECONDS)
                 .subscribe(value -> {
                     Map values = new HashMap<String, Object>();
                     values.put("temp", System.currentTimeMillis());
