d346980b6e888c768627df2e0d9ab1b31a81a549
Jakob Rathberger <jakob.rathberger@gmail.com>
2022-04-28 09:17:14 +0200

    persist entity room #leoiot-111

--- src/main/java/at/htl/mqtt/client/entity/Room.java
+++ src/main/java/at/htl/mqtt/client/entity/Room.java
@@ -1,13 +1,26 @@
 package at.htl.mqtt.client.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Room {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private String floor;
 
     public Room(String name, String floor) {
         this.name = name;
         this.floor = floor;
+    }
+
+    public Room() {
+
     }
 
     public String getName() {
