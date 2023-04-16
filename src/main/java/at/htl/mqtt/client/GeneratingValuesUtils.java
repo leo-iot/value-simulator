389a6684f95b2d1e756914d898e2b9fe52966bb9
philippkerschbaum <philipp.kerschbaum2@gmail.com>
2023-04-16 13:23:49 +0200

    improved algorithm

--- src/main/java/at/htl/mqtt/client/GeneratingValuesUtils.java
+++ src/main/java/at/htl/mqtt/client/GeneratingValuesUtils.java
@@ -8,11 +8,13 @@ import io.smallrye.reactive.messaging.mqtt.MqttMessage;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.json.JSONObject;
 
+import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.*;
 
 public class GeneratingValuesUtils {
     private static List<Double> goodTemps = new LinkedList<>();
+    private static final int AMOUNT_OF_ITERATIONS = 60;
 
     public static boolean checkSendingValues() {
         List<Config> c = Config.listAll();
@@ -23,60 +25,117 @@ public class GeneratingValuesUtils {
 
     @Scheduled(every = "60s")
     public void getValuesForRoom1() {
-        List<Room> rooms = Room.listAll();
-        getValueForRoom(rooms.get(0));
+        //List<Room> rooms = Room.listAll();
+        List<Value> vs = getValuesForRoom();
     }
 
 
-    public static void getValueForRoom(Room room) {
-        int index = 0;
+    @Transactional
+    public static List<Value> getValuesForRoom() {
+        List<Room> rooms = Room.listAll();
+        Room room = rooms.get(1);
 
-        System.out.println("\ncurrentValue" + " / " +
-                "MinValue" + " / " +
-                "MaxValue" + " ==> " +
-                "Name");
-        System.out.println("---------------------");
+        int index = 0;
+        Random rand = new Random();
 
         for (Value v : room.getValues()) {
 
-            double currentValue = getIntValue(
+            // PREPARE VALUE
+            if (v.getLastFullValue() == 0) {
+                v.setLastFullValue(
+                        rand.nextDouble(
+                                v.getValueType().getMinValue(),
+                                v.getValueType().getMaxValue()
+                        ));
+            }
+            // update next Full-Value when not set
+            if (v.getNextFullValue() == 0) {
+                v.setNextFullValue(
+                        rand.nextDouble(
+                                v.getValueType().getMinValue(),
+                                v.getValueType().getMaxValue()
+                        ));
+            }
+            if (v.getAmountOfIterations() != AMOUNT_OF_ITERATIONS) {
+                v.setAmountOfIterations(AMOUNT_OF_ITERATIONS);
+            }
+
+            double currYValue = calcPointOnStraight(
+                    v.getLastFullValue(), // Y-1
+                    v.getAmountOfIterations(), // X-2
+                    v.getNextFullValue(), // Y-2
+                    v.getIterationsCount() // X-Current
+            );
+
+            double currentDeviatingPoint = getIntDeviation(
                     v.getValueType().getMinValue(),
                     v.getValueType().getMaxValue(),
-                    v.getLastValue()
+                    currYValue
             );
-            room.getValues().get(index).setLastValue(currentValue);
 
-            System.out.println(currentValue + " / " +
-                    v.getValueType().getMinValue() + " / " +
-                    v.getValueType().getMaxValue() + " ==> " +
-                    v.getValueType().getName());
+            if (v.getIterationsCount() == v.getAmountOfIterations()) {
+                // reset count
+                v.setIterationsCount(0);
+
+                // update next value to random
+                v.setNextFullValue(
+                        rand.nextDouble(
+                                v.getValueType().getMinValue(),
+                                v.getValueType().getMaxValue()
+                        ));
+                // set "last"-value to current
+                v.setLastFullValue(currYValue);
+
+            } else {
+                // Increase count
+                v.setIterationsCount(v.getIterationsCount() + 1);
+            }
+
+            //room.persist();
+            room.getValues().get(index).setLastValue(currentDeviatingPoint);
             index++;
         }
-        System.out.println("---------------------");
-        room.persist();
+        return room.getValues();
     }
 
 
-    public static Double getIntValue(int min, int max, double previous) {
+    // returns the Y-Value of a given X-Value
+    // on a straight between 2 given Points
+    public static Double calcPointOnStraight(
+            double y1, // X-1 = 0
+            int x2, double y2,
+            int xCurrent) {
+        int x1 = 0;
+
+        double k = (y2 - y1) / (x2 - x1);
+
+        // y(x) = k * x + d
+        // y2 = k * x2 + d     // - (k * x2)
+        // d = y2 - (k * x2)
+        double d = y2 - (k * x2);
+
+        // y(x) = k * x + d
+        return k * xCurrent + d;
+    }
+
+    // returns a 10-percent deviation of value
+    public static Double getIntDeviation(int min, int max, double value) {
         Random rand = new Random();
 
+        // if value is not set -> set value to random value
+        if (value <= 0 && min <= 0) return rand.nextDouble(min, max);
 
-        if (previous <= 0 && min <= 0) {
-            return rand.nextDouble(min, max);
-        }
+        // Check if value is valid
+        if (value < min) value = min;
+        if (value > max) value = max;
 
-
-        // Check if previous value is valid
-        if (previous < min) previous = min;
-        if (previous > max) previous = max;
-
-        double temp = previous * 0.10;
+        double temp = value * rand.nextDouble(0.01, 0.10);
 
         // Randomize if value increases/decreases
         boolean subtract = rand.nextBoolean();
 
-        double subtractedValue = previous - temp;
-        double addedValue = previous + temp;
+        double subtractedValue = value - temp;
+        double addedValue = value + temp;
 
         // Check if CURRENT value is valid
         if (subtractedValue < min) subtractedValue = min;
