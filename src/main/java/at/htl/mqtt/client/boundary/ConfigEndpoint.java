40eaef8a47622bef2d469c18b1c2a34be6397b14
philippkerschbaum <philipp.kerschbaum2@gmail.com>
2023-04-16 13:23:28 +0200

    display regression manhatten distance - for better traceability of algorithm

--- src/main/java/at/htl/mqtt/client/boundary/ConfigEndpoint.java
+++ src/main/java/at/htl/mqtt/client/boundary/ConfigEndpoint.java
@@ -1,8 +1,10 @@
 package at.htl.mqtt.client.boundary;
 
+import at.htl.mqtt.client.GeneratingValuesUtils;
 import at.htl.mqtt.client.dto.ValueTypesDTO;
 import at.htl.mqtt.client.entity.Config;
 import at.htl.mqtt.client.entity.Room;
+import at.htl.mqtt.client.entity.Value;
 import at.htl.mqtt.client.entity.ValueType;
 import io.quarkus.qute.CheckedTemplate;
 import io.quarkus.qute.TemplateInstance;
@@ -11,8 +13,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Path("view")
 public class ConfigEndpoint {
@@ -20,10 +21,11 @@ public class ConfigEndpoint {
     @CheckedTemplate
     public static class Templates {
         public static native TemplateInstance temperatures(
-                List<Integer> temps,
+                List<Double> temps,
                 List<Room> rooms,
                 List<ValueType> types,
-                List<Config> config
+                List<Config> config,
+                List<Double> tempsMidLane
         );
     }
 
@@ -32,18 +34,29 @@ public class ConfigEndpoint {
     public TemplateInstance get() {
         List<Room> rooms = Room.listAll();
         List<Config> c = Config.listAll();
-        List<Integer> myNum = new ArrayList<>();
-        myNum.add(12);
-        myNum.add(15);
-        myNum.add(16);
-        myNum.add(18);
-        myNum.add(18);
+        List<Double> myNum = new ArrayList<>();
+        List<Double> tempsMidLane = new ArrayList<>();
+
+        List<Value> values = new ArrayList<>();
+
+        // Generate Test Data
+        for (int i = 0; i < 240; i++) {
+            values = GeneratingValuesUtils.getValuesForRoom();
+
+            for (Value v : values) {
+                if (Objects.equals(v.getValueType().getName(), "temperature")) {
+                    myNum.add(v.getLastValue());
+                    tempsMidLane.add(v.getNextFullValue());
+                }
+            }
+        }
 
         return Templates.temperatures(
                 myNum,
                 rooms,
                 ValueType.listAll(),
-                c
+                c,
+                tempsMidLane
         );
     }
 }
